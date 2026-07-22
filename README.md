a58e7f28a6f99a04c0524a36098d540bd9f550e2
KalengaAO <antoniopedrocalengar7@gmai.com>
2026-07-22 19:41:32 +0100

    testando o darshboard

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ os conteiner serão testar manual.
 
 docker run -it -p -v image 
 
-root@organon:/home/xxxx/Documents/Agenda-medica/project# docker exec -it flask bash
+root@organon:/home/org/Documents/Agenda-medica/project# docker exec -it flask bash
 
 root@b657a9945b4c:/app/tools# printenv SECRET_KEY
 123456789
@@ -35,3 +35,13 @@ sqlite> SELECT id, username, email FROM usuarios;
 1|medico.teste|medico.teste@timesaver.com.br
 
 
+root@organon:/home/org/Documents/Agenda-medica/project# docker exec -it api bash
+
+testando a API
+apt-get update && apt-get install iproute2 procps
+
+root@b1f0b01f06e2:/app/tools# ss -ltn
+
+State        Recv-Q       Send-Q              Local Address:Port                Peer Address:Port       
+LISTEN       0            4096                   127.0.0.11:35155                    0.0.0.0:*          
+LISTEN       0            2048                      0.0.0.0:5001                     0.0.0.0:*   
--- project/src/flask/tools/app/static/js/main.js
+++ project/src/flask/tools/app/static/js/main.js
@@ -1,1 +1,97 @@
 console.log("Agenda Médica carregada");
+
+
+async function loadAppointments() {
+
+    const container = document.getElementById("appointments");
+
+    if (!container) {
+        return;
+    }
+
+
+    try {
+
+        const response = await fetch(
+            "http://localhost:5001/appointments"
+        );
+
+
+        if (!response.ok) {
+            throw new Error(
+                "Erro ao carregar agendamentos"
+            );
+        }
+
+
+        const appointments = await response.json();
+
+
+        container.innerHTML = "";
+
+
+        appointments.forEach(item => {
+
+
+            const card = document.createElement("div");
+
+            card.className = "appointment";
+
+
+            card.innerHTML = `
+
+                <h3>${item.paciente}</h3>
+
+                <p>
+                    Médico:
+                    ${item.medico}
+                </p>
+
+                <p>
+                    Especialidade:
+                    ${item.especialidade}
+                </p>
+
+                <p>
+                    Data:
+                    ${item.data}
+                    ${item.horario}
+                </p>
+
+                <p>
+                    Convênio:
+                    ${item.convenio}
+                </p>
+
+                <p>
+                    Status:
+                    ${item.status}
+                </p>
+
+            `;
+
+
+            container.appendChild(card);
+
+        });
+
+
+    } catch(error) {
+
+        console.error(
+            "Erro:",
+            error
+        );
+
+
+        container.innerHTML =
+            "<p>Erro ao carregar agenda.</p>";
+    }
+
+}
+
+
+document.addEventListener(
+    "DOMContentLoaded",
+    loadAppointments
+);
